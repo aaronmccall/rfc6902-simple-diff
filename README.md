4cbeb5fd473f6009e4ce4e55ec49f6478b56c6cc
Aaron McCall <aaron@andyet.net>
2014-09-23 11:35:52 -0700

    make it super simple

--- README.md
+++ README.md
@@ -1,27 +1,74 @@
-rfc6902 diff generator
+rfc6902 simple diff generator
 ======================
 
-Another json diff tool.
+An extremely simple JSON patch tool that generates rfc6902 compliant add, remove, and replace operations only.
 
-If you are looking for a whole implementation(differ and patcher), please check @chbrown's [rfc6902](https://www.npmjs.org/package/rfc6902).
+If you are looking for a complete implementation(differ and patcher), please check @chbrown's [rfc6902](https://www.npmjs.org/package/rfc6902) or Cujo.js's [jiff](https://www.npmjs.org/package/jiff).
 
-Usage
+Getting started
 -----
 
 ```shell
-npm install rfc6902-diff
+npm install rfc6902-simple-diff
 ```
 
-### Develop
+```javascript
 
-```shell
-npm install
-npm run dev
+var simpleDiff = require('rfc6902-simple-diff');
+
+var lhs = {
+    foo: "bar",
+    baz: { bar: "foo" },
+    qux: ["foo", "bar"]
+};
+
+var rhs = {
+    foo: "baz",
+    baz: { qux: "bar" },
+    qux: ["foo","baz", "biz"]
+};
+
+console.log(simpleDiff(lhs, rhs));
+
+
+// Outputs:
+[
+    { op: 'replace', path: '/foo', value: 'baz' },
+    { op: 'remove', path: '/baz/bar' },
+    { op: 'add', path: '/baz/qux', value: 'bar' },
+    { op: 'replace', path: '/qux/1', value: 'baz' },
+    { op: 'add', path: '/qux/-', value: 'biz' }
+]
 ```
 
-### Test
+Customizing output
+------
 
-```shell
-npm install
-npm run test
+You can call the diff function with optional options object to customize output.
+
+
+```javascript
+
+console.log(simpleDiff(lhs, rhs, {path: 'bub'}));
+
+
+// Outputs:
+[
+    { op: 'replace', path: '/bub/foo', value: 'baz' },
+    { op: 'remove', path: '/bub/baz/bar' },
+    { op: 'add', path: '/bub/baz/qux', value: 'bar' },
+    { op: 'replace', path: '/bub/qux/1', value: 'baz' },
+    { op: 'add', path: '/bub/qux/2', value: 'biz' }
+]
+
+console.log(simpleDiff(lhs, rhs, {append: false}))
+
+// Outputs:
+[
+    { op: 'replace', path: '/foo', value: 'baz' },
+    { op: 'remove', path: '/baz/bar' },
+    { op: 'add', path: '/baz/qux', value: 'bar' },
+    { op: 'replace', path: '/qux/1', value: 'baz' },
+    { op: 'add', path: '/qux/2', value: 'biz' }
+]
 ```
---
+++ test/test.js
@@ -0,0 +1,139 @@
+var chai = require('chai');
+chai.should();
+var expect = chai.expect;
+var diff = require('../');
+
+describe('In RFC 6902,', function () {
+  describe('path', function (_) {
+    it('should start at "/" if no path in options', function () {
+      var patch = diff({foo: 1}, null)[0];
+      patch.op.should.equal('replace');
+      patch.path.should.equal('/');
+    });
+    it('should start at "/" + options.path if path in options', function () {
+      var patch = diff({foo: 1}, {foo: 2}, {path: 'biz'})[0];
+      patch.op.should.equal('replace');
+      patch.path.should.equal('/biz/foo');
+    });
+  });
+  describe("add", function (_) {
+    it("should happen when a new value has been added", function () {
+      var patch = diff({}, {
+        foo: {
+          foo: 'bar'
+        }
+      })[0];
+      patch.op.should.equal('add');
+      patch.path.should.equal('/foo');
+      patch.value.should.deep.equal({
+        foo: 'bar'
+      });
+    });
+    it('should have "-" as final node when append is not false', function () {
+        var patch = diff({foo: []}, {foo: [1]})[0];
+        patch.op.should.equal('add');
+        patch.path.should.equal('/foo/-');
+        patch.value.should.equal(1);
+    });
+    it('should have the index as final node when append is false', function () {
+        var patch = diff({foo: []}, {foo: [1]}, {append: false})[0];
+        patch.op.should.equal('add');
+        patch.path.should.equal('/foo/0');
+        patch.value.should.equal(1);
+    });
+  });
+  describe("remove", function (_) {
+    return it("should happen when a value has been removed", function () {
+      var patch = diff({
+        foo: {
+          foo: 'bar'
+        }
+      }, {})[0];
+      patch.op.should.equal('remove');
+      patch.path.should.equal('/foo');
+    });
+  });
+  describe("replace", function (_) {
+    return it("should happen when a value has been replaced", function () {
+      var patch = diff({
+        foo: 'bar'
+      }, {
+        foo: 'foobar'
+      })[0];
+      patch.op.should.equal('replace');
+      patch.path.should.equal('/foo');
+      patch.value.should.equal('foobar');
+    });
+  });
+  describe("even NaN and Infinity are not part of JSON,", function (_) {
+    it("NaN should be equal to NaN", function () {
+      var patch = diff({
+        foo: NaN
+      }, {
+        foo: NaN
+      });
+      patch.should.have.length(0);
+    });
+    return it("Infinity should be equal to Infinity", function () {
+      var patch = diff({
+        foo: Infinity
+      }, {
+        foo: Infinity
+      });
+      patch.should.have.length(0);
+    });
+  });
+
+  return describe("examples in Appendix A.", function (_) {
+    it("1.", function () {
+      var patch = diff({
+        foo: 'bar'
+      }, {
+        baz: 'qux',
+        foo: 'bar'
+      })[0];
+      patch.op.should.equal('add');
+      patch.path.should.equal('/baz');
+      patch.value.should.equal('qux');
+    });
+    it("3.", function () {
+      var patch = diff({
+        baz: 'qux',
+        foo: 'bar'
+      }, {
+        foo: 'bar'
+      })[0];
+      patch.op.should.equal('remove');
+      patch.path.should.equal('/baz');
+      return expect(patch).not.to.have.property('value');
+    });
+    it("5.", function () {
+      var patch = diff({
+        baz: 'qux',
+        foo: 'bar'
+      }, {
+        baz: 'boo',
+        foo: 'bar'
+      })[0];
+      patch.op.should.equal('replace');
+      patch.path.should.equal('/baz');
+      patch.value.should.equal('boo');
+    });
+    it("10.", function () {
+      var patch = diff({
+        foo: 'bar'
+      }, {
+        foo: 'bar',
+        child: {
+          grandchild: {}
+        }
+      });
+      patch = patch[0];
+      patch.op.should.equal('add');
+      patch.path.should.equal('/child');
+      patch.value.should.deep.equal({
+        grandchild: {}
+      });
+    });
+  });
+});
